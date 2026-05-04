7eed077f8fef6d239f96456c1f139f3d76249f35
Nitinjr812 <nitinjr812@gmail.com>
2026-05-04 14:41:41 +0530

    adding autyh

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+
 
 const Auth = () => {
-  return (
-    <>
-      <h1>hii</h1>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <div className="forms">
+                    <input type="text" placeholder="enter name " autoComplete="none" />
+                    <input type="email" name="" id="" />
+                    <input type="password" placeholder="enter password " />
+                    <button>Signup</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Auth
