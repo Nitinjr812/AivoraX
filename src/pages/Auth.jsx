5d8d37205a93ae86b7dde5a998e3d086f75610d1
Nitinjr812 <nitinjr812@gmail.com>
2026-05-04 14:49:23 +0530

    adding autyh

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 
 
 const Auth = () => {
+    const [user, setuser] = useState({})
+    const signup = (e) => {
+        setuser({
+            ...user, [e.target.name]: e.target.value
+        })
+        console.log(user)
+    }
     return (
         <>
             <div className="container">
                 <div className="forms">
-                    <input type="text" placeholder="enter name " autoComplete="none" />
-                    <input type="email" name="" id="" />
-                    <input type="password" placeholder="enter password " />
+                    <input type="text" name="name" onChange={signup} placeholder="enter name " autoComplete="none" />
+                    <input type="email"  onChange={signup}   name="email" id="" />
+                    <input type="password"  onChange={signup}   name="password" id="" />
+
                     <button>Signup</button>
                 </div>
             </div>
